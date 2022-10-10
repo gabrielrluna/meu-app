54a8d4b57e68e55d7288fc16ff1830ae651129b9
Gabriel Luna <lunagabri@gmail.com>
2022-10-10 12:00:10 -0300

    comp

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,9 +1,14 @@
 import Menu from "./Menu";
 
+const estilos = {
+  color: "white",
+  backgroundColor: "black",
+};
+
 const Cabecalho = () => {
   return (
-    <header style={{ backgroundColor: "lightblue" }}>
-      <h1>Interface React</h1>
+    <header style={{ backgroundColor: "lightblue", textAlign: "center" }}>
+      <h1 style={estilos}>Interface React</h1>
       <Menu />
     </header>
   );
