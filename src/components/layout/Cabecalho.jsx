6bc02b6fc3b61f99006d62dcf87e554476fb3e37
Gabriel Luna <lunagabri@gmail.com>
2022-10-11 11:27:20 -0300

    Styled Components

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,11 +1,35 @@
+import styled from "styled-components";
 import Menu from "./Menu";
+
+const periodo = "diurno";
+
+const StyledCabecalho = styled.header`
+  background-color: ${periodo === "noturno" ? "lightblue" : "darkblue"};
+  text-align: center;
+
+  h1 {
+    color: red;
+  }
+
+  p {
+    font-weight: bold;
+    color: blue;
+    font-size: 2rem;
+
+    &:hover {
+      color: red;
+      cursor: pointer;
+    }
+  }
+`;
 
 const Cabecalho = () => {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Interface React</h1>
       <Menu />
-    </header>
+      <p>Teste</p>
+    </StyledCabecalho>
   );
 };
 
