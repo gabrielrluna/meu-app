b4f61c3d129a2ab7500f87313e39ef22412f36ba
Gabriel Luna <lunagabri@gmail.com>
2022-10-13 11:50:11 -0300

    Update Rodape.jsx

--- src/components/layout/Rodape.jsx
+++ src/components/layout/Rodape.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 const Rodape = () => {
+  const estilosInicias = {
+    textAlign: "center",
+    textTransform: "uppercase",
+    border: "solid 2px red",
+  };
+
+  const [estilos, setEstilos] = useState(estilosInicias);
+
+  const esconder = () => setEstilos({ display: "none" });
+  const exibir = () => setEstilos(estilosInicias);
+
   return (
-    <footer>
-      <p>Rodapé</p>
-    </footer>
+    <>
+      <p>
+        <span onMouseOver={esconder}>Esconder</span> |
+        <span onMouseOver={exibir}>Exibir</span>
+      </p>
+      <footer style={estilos}>
+        <p>Rodapé</p>
+      </footer>
+    </>
   );
 };
 
