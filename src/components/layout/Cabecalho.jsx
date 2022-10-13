8888789f903fcf2f7efc203eb2d9081c4c0df11b
Gabriel Luna <lunagabri@gmail.com>
2022-10-13 11:34:02 -0300

    Usando useStates

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Menu from "./Menu";
 
 const Cabecalho = () => {
+  const exemplo2 = () => {
+    console.log("exemplo2");
+  };
+  // Aqui, definimos no useState um valor inicial (colocado entre os parênteses)
+  const [titulo, setTitulo] = useState("Exemplo de State");
+
+  const atualizaTitulo = () => {
+    setTitulo("Opa! state do título foi alterado");
+  };
+
   return (
     <header>
-      <h1>Interface React</h1>
+      <button onClick={() => console.log("exemplo1")}>Exemplo 1</button>
+      <button onClick={exemplo2}>Exemplo 2</button>
+      <hr />
+
+      {/*Aqui, aplicamos a variável de state chamada título*/}
+      <h1 onClick={atualizaTitulo}>{titulo}</h1>
       <Menu />
     </header>
   );
--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,10 +1,30 @@
 import Artigo from "../Artigo";
+import { useState } from "react";
+
 const Conteudo = () => {
+  const [corFundo, setCorFundo] = useState();
+  const [contador, setContador] = useState(0);
+
+  const fundoAmarelo = () => setCorFundo("#FFEE71");
+  const fundoAzul = () => setCorFundo("lightblue");
+  const fundoVermelho = () => setCorFundo("lightpink");
+
+  const atualizaContagem = () => setContador(contador + 1);
+
   return (
-    <main>
+    <main style={{ backgroundColor: corFundo }}>
+      <button onMouseOver={fundoAmarelo}>Amarelo</button>
+      <button onMouseOver={fundoAzul}>Azul</button>
+      <button onMouseOver={fundoVermelho}>Vermelho</button>
+
       <section>
-        <h2>Conteúdo do Site</h2>
-        <p>Lorem ipsum Dolor sit amet consectetur, adipisicing</p>
+        <h2 onClick={atualizaContagem}>Contador: {contador}</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ab
+          inventore, sapiente impedit numquam maxime quia voluptates, et at modi
+          harum recusandae? Inventore illo consectetur, quisquam distinctio
+          commodi voluptate fugit.
+        </p>
 
         <Artigo />
         <Artigo />
