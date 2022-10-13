77d6ec6bc10a7c441b8d61c030e9faf2b6e2db54
Gabriel Luna <lunagabri@gmail.com>
2022-10-13 09:32:31 -0300

    cimo

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -3,10 +3,25 @@ const Artigo = (props) => {
     <article>
       <h3>{props.titulo}</h3>
       <h4>{props.subtitulo}</h4>
-      <h4>{props.curso}</h4>
+      <h4>Curso: {props.curso}</h4>
       <p> {props.children} </p>
     </article>
   );
 };
 
 export default Artigo;
+
+// Versão Desestruturada (destructure)
+
+/*const Artigo = ({titulo, subtitulo, curso, children}) => {
+  return (
+    <article>
+      <h3>{titulo}</h3>
+      <h4>{subtitulo}</h4>
+      <h4>Curso: {curso}</h4>
+      <p> {children} </p>
+    </article>
+  );
+};
+
+export default Artigo;*/
--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,16 +1,13 @@
-import Menu from "./Menu";
+import Menu from "../Menu";
 import Button from "react-bootstrap/Button";
 
 const Cabecalho = () => {
   return (
-    <>
+    <header>
       <Button variant="danger">Teste</Button>
-
-      <header>
-        <h1>Interface React</h1>
-        <Menu />
-      </header>
-    </>
+      <h1>Interface React</h1>
+      <Menu />
+    </header>
   );
 };
 
