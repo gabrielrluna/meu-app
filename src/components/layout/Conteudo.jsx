973e2d7e43e44d14799737bf554ba00148dba30b
Gabriel Luna <lunagabri@gmail.com>
2022-10-13 10:01:30 -0300

    Update Conteudo.jsx

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -3,18 +3,21 @@ import Artigo from "../Artigo";
 const Conteudo = () => {
   const artigos = [
     {
+      id: 1,
       titulo: "Artigo 1",
       subtitulo: "Este é o subtitulo do artigo 1",
       texto: "Este é o conteúdo blablablablablablablabla do artigo 1",
       curso: "JS Avançado",
     },
     {
+      id: 2,
       titulo: "Artigo 2",
       subtitulo: "Este é o subtitulo do artigo 2",
       texto: "Este é o conteúdo blablablablablablablabla do artigo 2",
       curso: "React",
     },
     {
+      id: 3,
       titulo: "Artigo 3",
       subtitulo: "Este é o subtitulo do artigo 3",
       texto: "Este é o conteúdo blablablablablablablabla do artigo 3",
@@ -35,18 +38,17 @@ const Conteudo = () => {
           commodi voluptate fugit.
         </p>
 
-        {artigos.map((artigo) => {
-          return (
-            <Artigo
-              titulo={artigo.titulo}
-              subtitulo={artigo.subtitulo}
-              curso={artigo.curso}
-            >
-              {artigo.texto}
-              <p>===================================</p>
-            </Artigo>
-          );
-        })}
+        {artigos.map((artigo) => (
+          <Artigo
+            key={artigo.id}
+            titulo={artigo.titulo}
+            subtitulo={artigo.subtitulo}
+            curso={artigo.curso}
+          >
+            {artigo.texto}
+            <p>===================================</p>
+          </Artigo>
+        ))}
       </section>
     </main>
   );
